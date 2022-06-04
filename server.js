8e392f9b9daa010abf9336ee8f66e0290f98100f
erfan nouri <erfan.nouri@gmail.com>
2022-06-04 21:21:44 +0430

    cahnge to graphql-ws

--- server.js
+++ server.js
@@ -1,41 +1,62 @@
-// @ts-check
 const { createServer } = require("http");
 const express = require("express");
-const { execute, subscribe } = require("graphql");
-const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 const { PubSub } = require("graphql-subscriptions");
-const { SubscriptionServer } = require("subscriptions-transport-ws");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { WebSocketServer } =require ('ws');
+const { useServer } =require ('graphql-ws/lib/use/ws');
 const typeDefs = require("./src/types");
 const resolvers = require("./src/resolver");
-const db = require("./src/db");
-(async () => {
-  const pubsub = new PubSub();
 
-  const PORT = 4000;
+(async () => {
+
   const app = express();
   const httpServer = createServer(app);
 
   const schema = makeExecutableSchema({ typeDefs, resolvers });
+  
+  const wsServer = new WebSocketServer({
+    // This is the `httpServer` returned by createServer(app);
+    server: httpServer,
+    // Pass a different path here if your ApolloServer serves at
+    // a different path.
+    path: "/graphql",
+  });
+
+  // Passing in an instance of a GraphQLSchema and
+  // telling the WebSocketServer to start listening
+  const serverCleanup = useServer({ schema }, wsServer);
+
 
   const server = new ApolloServer({
     schema,
+    plugins: [
+      // Proper shutdown for the WebSocket server.
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
 
   });
+
+
+
   await server.start();
   server.applyMiddleware({ app });
 
-  SubscriptionServer.create(
-    { schema, execute, subscribe },
-    { server: httpServer, path: server.graphqlPath }
-  );
 
-  httpServer.listen(PORT, () => {
+  httpServer.listen(3000, () => {
     console.log(
-      `🚀 Query endpoint ready at http://localhost:${PORT}${server.graphqlPath}`
+      `🚀 Query endpoint ready at http://localhost:3000${server.graphqlPath}`
     );
     console.log(
-      `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`
+      `🚀 Subscription endpoint ready at ws://localhost:3000${server.graphqlPath}`
     );
   });
 })();
